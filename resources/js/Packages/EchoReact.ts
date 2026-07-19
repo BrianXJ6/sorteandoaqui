f8f0cef1f1c28895d6a832e889da4fcee079bb4e
Brian Barros <brianferreira15@hotmail.com>
2026-07-19 17:13:58 -0300

    Create config Echo file

---
+++ resources/js/Packages/EchoReact.ts
@@ -0,0 +1,12 @@
+import { configureEcho } from '@laravel/echo-react';
+
+configureEcho({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+    wsHost: import.meta.env.VITE_PUSHER_HOST,
+    wsPort: import.meta.env.VITE_PUSHER_PORT,
+    wssPort: import.meta.env.VITE_PUSHER_PORT,
+    enabledTransports: ['ws', 'wss'],
+});
