3240dbd1b934d056fe87b0add417bd521b6c0c57
Brian Barros <brianferreira15@hotmail.com>
2026-07-19 01:26:00 -0300

    add example in echo.js

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,15 +1,17 @@
-import Echo from 'laravel-echo';
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
 
-import Pusher from 'pusher-js';
 window.Pusher = Pusher;
-
 window.Echo = new Echo({
     broadcaster: "pusher",
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: true,
+    forceTLS: false,
     wsHost: import.meta.env.VITE_PUSHER_HOST,
     wsPort: import.meta.env.VITE_PUSHER_PORT,
     wssPort: import.meta.env.VITE_PUSHER_PORT,
     enabledTransports: ["ws", "wss"],
 });
+
+// Example connection
+// window.Echo.channel("test-chanel").listen(".test-event", (data) => console.log(data));
